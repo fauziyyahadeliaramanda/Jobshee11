749153ccd83a86319934454c3077e2a36f11b1d1
faujiiiahhh <framandaadelia@gmail.com>
2023-11-09 10:17:21 +0700

    Modifikasi No2

--- NestedLoop_2341760145.java
+++ NestedLoop_2341760145.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 public class NestedLoop_2341760145{
         public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
 
-            int[][] temps = new int[5][7];
+            double[][] temps = new double [5][7];
 
-            for (int i = 0; i < temps.length; i++){
-                System.out.println("Kota ke-" + i);
-                for (int j = 0; j < temps[0].  length; j++){
-                    System.out.print("Hari ke-" + (j+1) + ": ");
-                    temps[i][j] = input.nextInt();
-             }
+                for (int i = 0; i < temps.length; i++){
+                    System.out.println("Kota ke-" + i);
+                    for (int j = 0; j < temps[0].length; j++){
+                        System.out.print("Hari ke-" + (j + 1) + ": ");
+                        temps[i][j] = scan.nextDouble();
+                    }
             System.out.println();
-        }
-        for (int i = 0; i < temps.length; i++){
-            System.out.print("Kota ke-" + (i+1) + ":");
-            for (int temp : temps[i]){
-                System.out.print(temps + " ");
             }
-            System.out.println();
-        }
-    }
-}            
+            for (int i = 0; i < temps.length; i++){
+                System.out.print("Kota ke-" + (i + 1) + ": ");
+                for (double temperature : temps[i]){
+                    System.out.print(temperature + " ");
+            }
+            double total = 0;
+            for (double temperature : temps[i]){
+                total += temperature;
+            }
+            double rataRata = total / temps[i].length;
+            System.out.println("Rata rata : " + rataRata);
+            }
+       } 
+     }
