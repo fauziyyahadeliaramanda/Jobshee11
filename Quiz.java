2182148d4ee5484ea491d6b9bbd512dcec1889d8
faujiiiahhh <framandaadelia@gmail.com>
2023-11-09 09:39:20 +0700

    Modifikasi 4

--- Quiz.java
+++ Quiz.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
-public class Quiz {
-    public static void main(String[] args){
-        Random random = new Random();
-        Scanner input = new Scanner(System.in);
+    public class Quiz {
+        public static void main(String[] args) {
+            Random random = new Random();
+            Scanner input = new Scanner(System.in);
 
-        char menu= 'y';
-        do{
-            int number = random.nextInt(10) + 1;
-            boolean success= false;
+            char menu = 'y';
             do{
-                System.out.print("Tebak angka(1-10):");
-                int answer = input.nextInt();
-                input.nextLine();
-                success= (answer==number);
-            }while(!success);
+                int number = random.nextInt(10)+1;
+                boolean success = false;
+
+                do{
+                    System.out.print("tebak angka (1-10) : ");
+                    int answer = input.nextInt();
+                    input.nextLine();
+
+                if (answer < number){
+                    System.out.println("Angka yang anda masukkan lebih kecil dari jawaban.");
+                }else if (answer > number){
+                    System.out.println("Angka yang anda masukkan lebih besar dari jawaban");
+                }else {
+                    System.out.println("Jawaban Anda benar!!");
+                    success = true;
+                }
+            }while (!success);
             System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
-            menu= input.nextLine().charAt(0);
-        }while(menu=='y' || menu=='Y');
+            menu = input.nextLine().charAt(0);
+            
+        }while (menu == 'y' || menu == 'Y');
     }
 }
